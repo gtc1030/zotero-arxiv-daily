e764d311e60004031f5df6b302a71ea1284bdad6
gtc1030 <72656561+gtc1030@users.noreply.github.com>
2026-04-26 20:50:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def get_arxiv_paper(query:str, keyword:str, link:str, debug:bool=False) -> list[
     # logger.info(f"Print {feed.entries}.")
     all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries]
     # logger.info(f"Print {all_paper_ids}.")
-    # bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
+    bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
     
     for i in range(0,len(all_paper_ids),50):
         search = arxiv.Search(id_list=i[i:i+50])
